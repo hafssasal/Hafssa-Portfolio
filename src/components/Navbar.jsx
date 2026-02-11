476ed5f373e50a2cdf454d02bc8c191808641db6
Hafssa Salmi <hafssasalmi54@gmail.com>
2026-02-11 21:38:06 +0100

    light mode modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import { FiMoon, FiSun, FiMenu, FiX, FiArrowRight } from "react-icons/fi"
+import { FiMenu, FiX, FiArrowRight } from "react-icons/fi"
 
 export default function Navbar() {
     const sideMenuRef = useRef();
@@ -9,37 +9,23 @@ export default function Navbar() {
     const openMenu = () => {
         sideMenuRef.current.style.transform = 'translateX(-16rem)';
     }
+
     const closeMenu = () => {
         sideMenuRef.current.style.transform = 'translateX(16rem)';
-    }
-    const toggleTheme = () => {
-
-        document.documentElement.classList.toggle('dark');
-
-        if (document.documentElement.classList.contains('dark')) {
-            localStorage.theme = 'dark';
-        } else {
-            localStorage.theme = 'light';
-        }
     }
 
     useEffect(() => {
 
         window.addEventListener('scroll', () => {
             if (scrollY > 50) {
-                navRef.current.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20');
-                navLinkRef.current.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/30', "dark:bg-transparent");
+                navRef.current.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+                navLinkRef.current.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
             } else {
-                navRef.current.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20');
-                navLinkRef.current.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/30', "dark:bg-transparent");
+                navRef.current.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+                navLinkRef.current.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
             }
         })
 
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-        }
     }, [])
 
     return (
@@ -49,36 +35,41 @@ export default function Navbar() {
                     Hafssa<span className="text-purple-500">.</span>
                 </a>
 
-                <ul ref={navLinkRef} className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50 font-Ovo dark:border dark:border-white/30 dark:bg-transparent ">
-                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#top">Accueil</a></li>
-                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#about">À propos</a></li>
-                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#services">Services</a></li>
-                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#work">Mes projets</a></li>
-                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#contact">Contact</a></li>
+                <ul
+                    ref={navLinkRef}
+                    className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50 font-Ovo"
+                >
+                    <li><a className='hover:text-gray-500 transition' href="#top">Accueil</a></li>
+                    <li><a className='hover:text-gray-500 transition' href="#about">À propos</a></li>
+                    <li><a className='hover:text-gray-500 transition' href="#services">Services</a></li>
+                    <li><a className='hover:text-gray-500 transition' href="#work">Mes projets</a></li>
+                    <li><a className='hover:text-gray-500 transition' href="#contact">Contact</a></li>
                 </ul>
 
                 <div className="flex items-center gap-4">
 
-                    <button onClick={toggleTheme} className="text-xl">
-                        <FiMoon className="dark:hidden"/>
-                        <FiSun className="hidden dark:block"/>
-                    </button>
-
-                    <a href="#contact" className="hidden lg:flex items-center gap-3 px-8 py-1.5 border border-gray-300 hover:bg-slate-100/70 dark:hover:bg-darkHover rounded-full ml-4 font-Ovo dark:border-white/30">
+                    <a
+                        href="#contact"
+                        className="hidden lg:flex items-center gap-3 px-8 py-1.5 border border-gray-300 hover:bg-slate-100/70 rounded-full ml-4 font-Ovo"
+                    >
                         Contact
-                        <FiArrowRight/>
+                        <FiArrowRight />
                     </a>
 
                     <button className="block md:hidden ml-3 text-xl" onClick={openMenu}>
-                        <FiMenu className="dark:hidden"/>
-                        <FiMenu className="hidden dark:block"/>
+                        <FiMenu />
                     </button>
                 </div>
 
-                <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 font-Ovo dark:bg-darkHover dark:text-white">
-
-                    <div className="absolute right-6 top-6 text-xl cursor-pointer" onClick={closeMenu}>
-                        <FiX/>
+                <ul
+                    ref={sideMenuRef}
+                    className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 font-Ovo"
+                >
+                    <div
+                        className="absolute right-6 top-6 text-xl cursor-pointer"
+                        onClick={closeMenu}
+                    >
+                        <FiX />
                     </div>
 
                     <li><a href="#top" onClick={closeMenu}>Accueil</a></li>
